b007591c42881a8abe02ab2f0a1715e8216c4bed
lobosabir5-source <tu_correo@gmail.com>
2026-09-12 01:26:16 -0400

    inicio

---
+++ src/test/java/com/example/Practica_aula/PracticaAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practica_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
